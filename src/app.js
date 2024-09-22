b43ae36e25794b70316c165697ce773e2a81fb8f
Rajat Das <rajatdas@MacBook-Air.local>
2024-09-22 17:47:48 +0530

    route handling, middleware, error handling

--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
 const express = require('express');
+const { adminAuth } = require('./middlewares/auth');
 
 const app = express();
 
-app.use((req, res) => {
-    if (req.url === '/test') {
-        res.send('Test server'); ``
+app.use('/', (err, req, res, next) => {
+    if (err) {
+        res.status(500).send('Something went wrong, contact support team !!!');
     }
-    if (req.url === '/local') {
-        res.send('Local server'); ``
-    }
-    res.send('Node Js server');
 })
+
+
+app.get('/user', (req, res) => {
+    try {
+        throw new Error('error');
+        res.send('all users'); // route handling
+    }
+    catch (err) {
+        res.status(500).send('Something went wrong !!!');
+    }
+
+})
+
+
+
+// app.use('/admin', adminAuth)
+
+// app.get('/user', (req, res) => {
+//     res.send('all users'); // route handling
+// })
+
+// app.get('/admin/getData', (req, res) => {
+//     res.send('all get data');
+// })
+// app.get('/admin/postData', (req, res) => {
+//     res.send('all post data')
+// })
 
 app.listen(3000, () => {
     console.log('listening to port 3000');
---
+++ src/middlewares/auth.js
@@ -0,0 +1,14 @@
+// Handling middleware
+const adminAuth = (req, res, next) => {
+    console.log('authencating');
+    const token = 'abc';
+    const checkAdmin = token === 'abc';
+    if (!checkAdmin) {
+        res.status(401).send('unauthorised access');
+    }
+    else {
+        next();
+    }
+}
+
+module.exports = { adminAuth };
