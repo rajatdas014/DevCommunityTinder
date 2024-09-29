e1238c63474b4eb166f8495e568bbe476ce4d245
Rajat Das <rajatdas@MacBook-Air.local>
2024-09-29 18:32:08 +0530

    Building Feed API & Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRouter = express.Router();
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user')
 
 const USER_SHARED_DATA = ["firstName", "lastName", "photoUrl", "age", "about", "skills"];
 
@@ -56,5 +57,44 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
         res.status(400).send('ERROR : ' + err.message);
     }
 });
+
+
+userRouter.get('/feed', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId")
+            .populate('fromUserId', 'firstName')
+            .populate('toUserId', 'firstName');
+
+        const hideConnections = new Set();
+        connectionRequests.forEach((val) => {
+            hideConnections.add(val.toUserId._id.toString());
+            hideConnections.add(val.fromUserId._id.toString());
+        })
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideConnections) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select(USER_SHARED_DATA).skip(skip).limit(limit);
+
+        res.send(users);
+    }
+    catch (err) {
+        res.status(400).send('ERROR : ' + err.message);
+    }
+})
 
 module.exports = userRouter;
